735c10ab909febc736dcd4f13882965b4768010a
artpap <100701975+artpap@users.noreply.github.com>
2023-03-22 19:20:33 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
-import { data } from './data';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          type='button'
+          className='btn btn-block'
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+}
 
-const { name, age, image } = data;
-
-const [person, setPerson] = useState(null);
-
-const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
 export default App;
